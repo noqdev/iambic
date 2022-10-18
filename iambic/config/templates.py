06419af58a1968e4f751512fd65b23f07133f282
Will-NOQ <102608210+Will-NOQ@users.noreply.github.com>
2022-10-18 08:53:16 -0500

    Started adding new ManagedPolicyTemplate. Model cleanup.

--- iambic/config/templates.py
+++ iambic/config/templates.py
@@ -1,6 +1,7 @@
-from iambic.aws.iam.role.models import MultiAccountRoleTemplate
+from iambic.aws.iam.role.models import RoleTemplate
+from iambic.google.models import GroupTemplate
 
-TEMPLATES = [MultiAccountRoleTemplate]
+TEMPLATES = [RoleTemplate, GroupTemplate]
 TEMPLATE_TYPE_MAP = {
     template.__fields__["template_type"].default: template for template in TEMPLATES
 }
