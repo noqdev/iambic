10c2824a74c6bc845cd380a487b05b2e7662acde
Will-NOQ <102608210+Will-NOQ@users.noreply.github.com>
2022-10-28 08:28:26 -0500

    Added new get_role function. Updated apply_role_managed_policies and apply_role_inline_policies to require existing policies as a param. Updated RoleTemplate._apply_to_account to use get_role.

--- iambic/aws/iam/role/utils.py
+++ iambic/aws/iam/role/utils.py
@@ -1,5 +1,6 @@
 import asyncio
 import json
+from typing import Union
 
 from deepdiff import DeepDiff
 
@@ -38,7 +39,9 @@ async def get_role_policy(role_name: str, policy_name: str, iam_client):
     )
 
 
-async def get_role_inline_policies(role_name: str, iam_client):
+async def get_role_inline_policies(
+    role_name: str, iam_client, as_dict: bool = True
+) -> Union[list | dict]:
     policy_names = await get_role_inline_policy_names(role_name, iam_client)
     policies = await asyncio.gather(
         *[
@@ -46,7 +49,13 @@ async def get_role_inline_policies(role_name: str, iam_client):
             for policy_name in policy_names
         ]
     )
-    return {policy["PolicyName"]: policy["PolicyDocument"] for policy in policies}
+    if as_dict:
+        return {policy["PolicyName"]: policy["PolicyDocument"] for policy in policies}
+    else:
+        return [
+            {"PolicyName": policy["PolicyName"], **policy["PolicyDocument"]}
+            for policy in policies
+        ]
 
 
 async def get_role_managed_policies(role_name: str, iam_client) -> list[dict[str, str]]:
@@ -65,6 +74,23 @@ async def get_role_managed_policies(role_name: str, iam_client) -> list[dict[str
             break
 
     return policies
+
+
+async def get_role(role_name: str, iam_client):
+    try:
+        current_role = (await aio_wrapper(iam_client.get_role, RoleName=role_name))[
+            "Role"
+        ]
+        current_role["ManagedPolicies"] = await get_role_managed_policies(
+            role_name, iam_client
+        )
+        current_role["InlinePolicies"] = await get_role_inline_policies(
+            role_name, iam_client, as_dict=False
+        )
+    except iam_client.exceptions.NoSuchEntityException:
+        current_role = {}
+
+    return current_role
 
 
 async def apply_role_tags(
@@ -184,20 +210,15 @@ async def apply_role_managed_policies(
     role_name,
     iam_client,
     template_policies: list[dict],
-    role_exists: bool,
+    existing_policies: list[dict],
     log_params: dict,
 ) -> list[ProposedChange]:
     tasks = []
     response = []
     template_policies = [policy["PolicyArn"] for policy in template_policies]
-    if role_exists:
-        managed_policies_resp = await get_role_managed_policies(role_name, iam_client)
-        existing_managed_policies = [
-            policy["PolicyArn"] for policy in managed_policies_resp
-        ]
-    else:
-        existing_managed_policies = []
+    existing_managed_policies = [policy["PolicyArn"] for policy in existing_policies]
 
+    # Create new managed policies
     new_managed_policies = [
         policy_arn
         for policy_arn in template_policies
@@ -225,6 +246,7 @@ async def apply_role_managed_policies(
             ]
         log.info(log_str, managed_policies=new_managed_policies, **log_params)
 
+    # Delete existing managed policies not in template
     existing_managed_policies = [
         policy_arn
         for policy_arn in existing_managed_policies
@@ -264,93 +286,94 @@ async def apply_role_inline_policies(
     role_name,
     iam_client,
     template_policies: list[dict],
-    role_exists: bool,
+    existing_policies: list[dict],
     log_params: dict,
 ) -> list[ProposedChange]:
     tasks = []
     response = []
+    template_policy_map = {
+        policy["PolicyName"]: {k: v for k, v in policy.items() if k != "PolicyName"}
+        for policy in template_policies
+    }
+    existing_policy_map = {
+        policy["PolicyName"]: {k: v for k, v in policy.items() if k != "PolicyName"}
+        for policy in existing_policies
+    }
 
-    if role_exists:
-        template_policy_map = {
-            policy["PolicyName"]: {k: v for k, v in policy.items() if k != "PolicyName"}
-            for policy in template_policies
-        }
-        existing_policy_map = await get_role_inline_policies(role_name, iam_client)
-
-        for policy_name in existing_policy_map.keys():
-            if not template_policy_map.get(policy_name):
-                log_str = "Stale inline policies discovered."
-                if ctx.execute:
-                    log_str = f"{log_str} Removing inline policy..."
-                    response.append(
-                        ProposedChange(
-                            change_type=ProposedChangeType.DELETE,
-                            resource_id=policy_name,
-                            attribute="inline_policies",
-                        )
+    for policy_name in existing_policy_map.keys():
+        if not template_policy_map.get(policy_name):
+            log_str = "Stale inline policies discovered."
+            if ctx.execute:
+                log_str = f"{log_str} Removing inline policy..."
+                response.append(
+                    ProposedChange(
+                        change_type=ProposedChangeType.DELETE,
+                        resource_id=policy_name,
+                        attribute="inline_policies",
                     )
-                    tasks.append(
-                        aio_wrapper(
-                            iam_client.delete_role_policy,
-                            RoleName=role_name,
-                            PolicyName=policy_name,
-                        )
+                )
+                tasks.append(
+                    aio_wrapper(
+                        iam_client.delete_role_policy,
+                        RoleName=role_name,
+                        PolicyName=policy_name,
                     )
-                log.info(log_str, policy_name=policy_name, **log_params)
+                )
+            log.info(log_str, policy_name=policy_name, **log_params)
 
-        for policy_name, policy_document in template_policy_map.items():
-            existing_policy_doc = existing_policy_map.get(policy_name)
-            policy_drift = None
-            if existing_policy_doc:
-                policy_drift = await aio_wrapper(
-                    DeepDiff,
-                    existing_policy_doc,
-                    policy_document,
-                    ignore_order=True,
+    for policy_name, policy_document in template_policy_map.items():
+        existing_policy_doc = existing_policy_map.get(policy_name)
+        policy_drift = None
+        if existing_policy_doc:
+            policy_drift = await aio_wrapper(
+                DeepDiff,
+                existing_policy_doc,
+                policy_document,
+                ignore_order=True,
+            )
+
+        if not existing_policy_doc or policy_drift:
+            if policy_drift:
+                log_params["policy_drift"] = policy_drift
+                boto_action = "Updating"
+                resource_existence = "Stale"
+                response.append(
+                    ProposedChange(
+                        change_type=ProposedChangeType.UPDATE,
+                        resource_id=policy_name,
+                        attribute="inline_policies",
+                        change_summary=policy_drift,
+                        current_value=existing_policy_doc,
+                        new_value=policy_document,
+                    )
+                )
+            else:
+                boto_action = "Creating"
+                resource_existence = "New"
+                response.append(
+                    ProposedChange(
+                        change_type=ProposedChangeType.CREATE,
+                        resource_id=policy_name,
+                        attribute="inline_policies",
+                        new_value=policy_document,
+                    )
                 )
 
-            if not existing_policy_doc or policy_drift:
-                if policy_drift:
-                    log_params["policy_drift"] = policy_drift
-                    boto_action = "Updating"
-                    resource_existence = "Stale"
-                    response.append(
-                        ProposedChange(
-                            change_type=ProposedChangeType.UPDATE,
-                            resource_id=policy_name,
-                            attribute="inline_policies",
-                            change_summary=policy_drift,
-                            current_value=existing_policy_doc,
-                            new_value=policy_document,
-                        )
+            log_str = f"{resource_existence} inline policies discovered."
+            if ctx.execute:
+                log_str = f"{log_str} {boto_action} inline policy..."
+                tasks.append(
+                    aio_wrapper(
+                        iam_client.put_role_policy,
+                        RoleName=role_name,
+                        PolicyName=policy_name,
+                        PolicyDocument=json.dumps(policy_document),
                     )
-                else:
-                    boto_action = "Creating"
-                    resource_existence = "New"
-                    response.append(
-                        ProposedChange(
-                            change_type=ProposedChangeType.CREATE,
-                            resource_id=policy_name,
-                            attribute="inline_policies",
-                            new_value=policy_document,
-                        )
-                    )
+                )
+            log.info(log_str, policy_name=policy_name, **log_params)
 
-                log_str = f"{resource_existence} inline policies discovered."
-                if ctx.execute:
-                    log_str = f"{log_str} {boto_action} inline policy..."
-                    tasks.append(
-                        aio_wrapper(
-                            iam_client.put_role_policy,
-                            RoleName=role_name,
-                            PolicyName=policy_name,
-                            PolicyDocument=json.dumps(policy_document),
-                        )
-                    )
-                log.info(log_str, policy_name=policy_name, **log_params)
-
-        if tasks:
-            await asyncio.gather(*tasks)
+    if tasks:
+        await asyncio.gather(*tasks)
 
     return response
 
