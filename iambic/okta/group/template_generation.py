1458d6d2cd53a4c7d6266b94845b411d7e2d7681
Curtis Castrapel <curtis@noq.dev>
2022-12-21 14:20:00 -0800

    Use BaseTemplate write functionality

--- iambic/okta/group/template_generation.py
+++ iambic/okta/group/template_generation.py
@@ -1,7 +1,5 @@
-import json
 import os
 
-from iambic.core.utils import yaml
 from iambic.okta.group.models import get_group_template
 from iambic.okta.group.utils import list_all_groups
 
@@ -16,21 +14,6 @@ async def generate_group_templates(config, output_dir, okta_organization):
     for okta_group in groups:
         group = await get_group_template(okta_group)
         file_path = os.path.expanduser(group.file_path)
-        path = os.path.join(base_path, file_path)
-        os.makedirs(os.path.dirname(path), exist_ok=True)
-        with open(path, "w") as f:
-            f.write(
-                yaml.dump(
-                    {
-                        "template_type": group.template_type,
-                        **json.loads(
-                            group.json(
-                                exclude_unset=True,
-                                exclude_defaults=True,
-                                exclude={"file_path"},
-                            )
-                        ),
-                    }
-                )
-            )
+        group.file_path = os.path.join(base_path, file_path)
+        group.write()
     return groups
