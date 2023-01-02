3c47637f0b3b46acf24568d36d0c18c66c4aea25
Will-NOQ <102608210+Will-NOQ@users.noreply.github.com>
2023-01-02 11:16:07 -0600

    Added basic functional tests for SSO permission sets.

---
+++ functional_tests/aws/permission_set/utils.py
@@ -0,0 +1,69 @@
+from __future__ import annotations
+
+import random
+
+from iambic.aws.models import AWSAccount
+from iambic.aws.sso.permission_set.models import (
+    AWS_SSO_PERMISSION_SET_TEMPLATE_TYPE,
+    AWSSSOPermissionSetTemplate,
+    PermissionSetAccess,
+)
+from iambic.aws.sso.permission_set.template_generation import get_permission_set_dir
+from iambic.core.utils import gather_templates
+
+
+def attach_access_rule(
+    permission_set_template: AWSSSOPermissionSetTemplate,
+    aws_account: AWSAccount,
+    exclude_accounts: int = 0,
+) -> AWSSSOPermissionSetTemplate:
+    users = [
+        details["UserName"] for details in aws_account.sso_details.user_map.values()
+    ]
+    groups = [
+        details["DisplayName"] for details in aws_account.sso_details.user_map.values()
+    ]
+    org_accounts = list(aws_account.sso_details.org_account_map.values())
+    if exclude_accounts:
+        exclude_accounts = min(exclude_accounts, len(org_accounts) - 1)
+
+    excluded_accounts = [] if not exclude_accounts else org_accounts[exclude_accounts:]
+    include_accounts = random.randint(0, len(org_accounts) - exclude_accounts)
+    if include_accounts == len(org_accounts) - 1:
+        included_accounts = ["*"]
+    else:
+        included_accounts = org_accounts[:include_accounts]
+
+    permission_set_template.access_rules.append(
+        PermissionSetAccess(
+            users=users[: random.randint(1, len(users))],
+            groups=users[: random.randint(1, len(groups))],
+            excluded_accounts=excluded_accounts,
+            included_accounts=included_accounts,
+        )
+    )
+
+    return permission_set_template
+
+
+async def generate_permission_set_template_from_base(
+    repo_dir: str,
+) -> AWSSSOPermissionSetTemplate:
+    permission_sets = await gather_templates(
+        repo_dir, AWS_SSO_PERMISSION_SET_TEMPLATE_TYPE
+    )
+    permission_set_dir = get_permission_set_dir(repo_dir)
+    permission_set_template = AWSSSOPermissionSetTemplate.load(
+        random.choice(permission_sets)
+    )
+    permission_set_template.identifier = f"iambic_test_{random.randint(0, 10000)}"
+    permission_set_template.file_path = (
+        f"{permission_set_dir}/{permission_set_template.identifier}.yaml"
+    )
+
+    permission_set_template.properties.name = permission_set_template.identifier
+    permission_set_template.properties.description = (
+        "This was created by a functional test."
+    )
+
+    return permission_set_template
