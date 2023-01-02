f07a2a4c325b0272a79f40b3631030b4ab98ab18
Will-NOQ <102608210+Will-NOQ@users.noreply.github.com>
2023-01-02 11:16:07 -0600

    Added basic functional tests for SSO permission sets.

---
+++ functional_tests/aws/permission_set/test_update_template.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+
+import asyncio
+from time import sleep
+from unittest import IsolatedAsyncioTestCase
+
+from functional_tests.aws.permission_set.utils import (
+    generate_permission_set_template_from_base,
+)
+from functional_tests.conftest import IAMBIC_TEST_DETAILS
+from iambic.core.context import ctx
+
+
+class UpdatePermissionSetTestCase(IsolatedAsyncioTestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.template = asyncio.run(
+            generate_permission_set_template_from_base(
+                IAMBIC_TEST_DETAILS.template_dir_path
+            )
+        )
+        asyncio.run(cls.template.apply(IAMBIC_TEST_DETAILS.config, ctx))
+        sleep(5)
+        asyncio.run(IAMBIC_TEST_DETAILS.sso_account.set_sso_details())
+
+    @classmethod
+    def tearDownClass(cls):
+        sleep(5)
+        asyncio.run(IAMBIC_TEST_DETAILS.sso_account.set_sso_details())
+        cls.template.deleted = True
+        asyncio.run(cls.template.apply(IAMBIC_TEST_DETAILS.config, ctx))
+
+    async def test_update_description(self):
+        self.template.properties.description = "Updated description"
+        await self.template.apply(IAMBIC_TEST_DETAILS.config, ctx)
+        await IAMBIC_TEST_DETAILS.sso_account.set_sso_details()
+
+        self.assertEqual(
+            self.template.properties.description,
+            IAMBIC_TEST_DETAILS.sso_account.sso_details.permission_set_map[
+                self.template.identifier
+            ]["Description"],
+        )
