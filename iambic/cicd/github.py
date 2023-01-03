aff5ccdfc4e6645d15afe23a071d2d70f3a991c0
Steven Moy <steven@noq.dev>
2023-01-02 20:53:30 -0800

    Add clone repo functionality

--- iambic/cicd/github.py
+++ iambic/cicd/github.py
@@ -3,11 +3,16 @@ from __future__ import annotations
 import json
 import os
 import sys
+from urllib.parse import urlparse
 
 from github import Github
 
+from iambic.core.git import clone_git_repo
+
 iambic_app = __import__("iambic.lambda.app", globals(), locals(), [], 0)
 lambda_run_handler = getattr(iambic_app, "lambda").app.run_handler
+lambda_repo_path = getattr(iambic_app, "lambda").app.REPO_BASE_PATH
+
 
 MERGEABLE_STATE_CLEAN = "clean"
 MERGEABLE_STATE_BLOCKED = "blocked"
@@ -22,13 +27,26 @@ def run_handler(context=None):
     handle_issue_comment(github_client, context)
 
 
+def format_github_url(repository_url, github_token):
+    parse_result = urlparse(repository_url)
+    return parse_result._replace(
+        netloc="oauth2:{0}@{1}".format(github_token, parse_result.netloc)
+    ).geturl()
+
+
 def handle_issue_comment(github_client, context):
-    # Get PR
+    # repo_name is already in the format {repo_owner}/{repo_short_name}
+    github_token = context.get("token", None)
     repo_name = context.get("repository", None)
     pull_number = context.get("event", {}).get("issue", {}).get("number")
-    # repo_name is already in the format {repo_owner}/{repo_short_name}
+    repository_url = context.get("event", {}).get("repository", {}).get("clone_url")
+    repo_url = format_github_url(repository_url, github_token)
+    # repository_url_token
     templates_repo = github_client.get_repo(repo_name)
     pull_request = templates_repo.get_pull(pull_number)
+    pull_request_branch_name = pull_request.head.ref
+    print("pull_request branch name is {0}".format(pull_request_branch_name))
+
     if pull_request.mergeable_state != MERGEABLE_STATE_CLEAN:
         # TODO log error and also make a comment to PR
         pull_request.create_issue_comment(
@@ -36,11 +54,20 @@ def handle_issue_comment(github_client, context):
         )
         return
     try:
+        cloned_repo = clone_git_repo(
+            repo_url, lambda_repo_path, pull_request_branch_name
+        )
+        print("closed_repo head: {}".format(cloned_repo.head.commit.hexsha))
+        # This is for fail safe, just in case.
+        assert cloned_repo.head.commit.hexsha == pull_request.head.sha
+
         lambda_run_handler(None, {"command": "git_apply"})
     except Exception as e:
         print(e)
         pull_request.create_issue_comment(
-            "exception during git-apply is {0}".format(pull_request.mergable_state)
+            "exception during git-apply is {0} \n {1}".format(
+                pull_request.mergable_state, e
+            )
         )
         return
     pull_request.merge()
