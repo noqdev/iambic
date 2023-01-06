d1f17dd5867c7db04083bd5a44f4dcc6ec7b9155
Steven Moy <steven@noq.dev>
2023-01-06 15:38:08 -0800

    Add TypeHints on functions

--- iambic/cicd/github.py
+++ iambic/cicd/github.py
@@ -3,14 +3,15 @@ from __future__ import annotations
 import json
 import os
 import shutil
-import sys
 from enum import Enum
+from typing import Any, Callable
 from urllib.parse import urlparse
 
 import yaml
-from github import Github
+from github import Github, PullRequest
 
-from iambic.core.git import clone_git_repo
+from iambic.core.git import Repo, clone_git_repo
+from iambic.core.logger import log
 
 iambic_app = __import__("iambic.lambda.app", globals(), locals(), [], 0)
 lambda_run_handler = getattr(iambic_app, "lambda").app.run_handler
@@ -31,23 +32,35 @@ class HandleIssueCommentReturnCode(Enum):
     MERGED = 4
 
 
-def run_handler(context=None):
-    github_token = context["token"]
-    print("event_name: {0}".format(context["event_name"]))
+# context is a dictionary structure published by Github Action
+# https://docs.github.com/en/actions/learn-github-actions/contexts#github-context
+def run_handler(context: dict[str, Any]):
+    github_token: str = context["token"]
+    event_name: str = context["event_name"]
+    log_params = {"event_name": event_name}
+    log.info("no op", **log_params)
     github_client = Github(github_token)
     # TODO Support Github Enterprise with custom hostname
     # g = Github(base_url="https://{hostname}/api/v3", login_or_token="access_token")
-    handle_issue_comment(github_client, context)
+
+    f: Callable[[Github, dict[str, Any]]] = EVENT_DISPATCH_MAP.get(event_name)
+    if f:
+        f(github_client, context)
+    else:
+        log.error("no supported handler")
+        raise Exception("no supported handler")
 
 
-def format_github_url(repository_url, github_token):
+def format_github_url(repository_url: str, github_token: str) -> str:
     parse_result = urlparse(repository_url)
     return parse_result._replace(
         netloc="oauth2:{0}@{1}".format(github_token, parse_result.netloc)
     ).geturl()
 
 
-def prepare_local_repo(repo_url, repo_path, pull_request_branch_name):
+def prepare_local_repo(
+    repo_url: str, repo_path: str, pull_request_branch_name: str
+) -> Repo:
     cloned_repo = clone_git_repo(repo_url, repo_path, None)
     for remote in cloned_repo.remotes:
         remote.fetch()
@@ -57,14 +70,15 @@ def prepare_local_repo(repo_url, repo_path, pull_request_branch_name):
     return cloned_repo
 
 
-def load_proposed_changes(filepath):
+def load_proposed_changes(filepath: str) -> dict:
     if not os.path.exists(filepath):
         return {}
     with open(filepath, "r") as f:
         return yaml.load(f)
 
 
-def format_proposed_changes(changes):
+# TODO do more formatting to emphasize resources deletion
+def format_proposed_changes(changes: dict) -> str:
     pass
 
 
@@ -78,7 +92,9 @@ GIT_APPLY_COMMENT_TEMPLATE = """iambic git-applied ran with:
 """
 
 
-def post_result_as_pr_comment(pull_request, context):
+def post_result_as_pr_comment(
+    pull_request: PullRequest, context: dict[str, Any]
+) -> None:
     run_url = (
         context.get("server_url")
         + "/"
@@ -101,7 +117,7 @@ def post_result_as_pr_comment(pull_request, context):
     pull_request.create_issue_comment(body)
 
 
-def copy_data_to_data_directory():
+def copy_data_to_data_directory() -> None:
     cwd = os.getcwd()
     filepath = f"{cwd}/proposed_changes.yaml"
     dest_dir = SHARED_CONTAINER_GITHUB_DIRECTORY
@@ -111,11 +127,14 @@ def copy_data_to_data_directory():
         shutil.copy(filepath, f"{dest_dir}/proposed_changes.yaml")
 
 
-def handle_issue_comment(github_client, context) -> HandleIssueCommentReturnCode:
+def handle_issue_comment(
+    github_client: Github, context: dict[str, Any]
+) -> HandleIssueCommentReturnCode:
 
     comment_body = context.get("event", {}).get("comment", {}).get("body")
     if comment_body != "iambic git-apply":
-        print("no op")
+        log_params = {"comment_body": comment_body}
+        log.info("no op", **log_params)
         return HandleIssueCommentReturnCode.NO_MATCHING_BODY
 
     github_token = context.get("token", None)
@@ -153,11 +172,13 @@ def handle_issue_comment(github_client, context) -> HandleIssueCommentReturnCode
         return HandleIssueCommentReturnCode.UNDEFINED
 
 
+EVENT_DISPATCH_MAP: dict[str, Callable] = {
+    "issue_comment": handle_issue_comment,
+}
+
+
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        raise Exception("You must pass a command")
-    command = sys.argv[1]
     github_context_json_str = os.environ.get("GITHUB_CONTEXT")
     with open("/root/github_context/github_context.json", "r") as f:
         github_context = json.load(f)
-    run_handler(context=github_context)
+    run_handler(github_context)
