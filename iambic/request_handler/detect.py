c1f33381b26dc42f00aaacced62c42694d3e2fcd
Will-NOQ <102608210+Will-NOQ@users.noreply.github.com>
2023-01-12 15:22:59 -0600

    Added testing for partial import on managed policy, role, and permission set. detect_changes now uses partial import.

--- iambic/request_handler/detect.py
+++ iambic/request_handler/detect.py
@@ -1,97 +1,178 @@
-# Intentionally hacky for demo
 from __future__ import annotations
 
+import asyncio
 import json
+from typing import Union
 
+from iambic.aws.event_bridge.models import (
+    ManagedPolicyMessageDetails,
+    PermissionSetMessageDetails,
+    RoleMessageDetails,
+)
+from iambic.aws.iam.policy.template_generation import (
+    generate_aws_managed_policy_templates,
+)
+from iambic.aws.iam.role.template_generation import generate_aws_role_templates
+from iambic.aws.identity_center.permission_set.template_generation import (
+    generate_aws_permission_set_templates,
+)
 from iambic.config.models import Config
 from iambic.core.logger import log
-from iambic.slack.notifications import send_iam_mutation_message
 
 
-async def detect_changes(config: Config) -> bool:
-    queue_arn = config.sqs.get("queues", {}).get("iam_mutation", {}).get("arn", "")
-    if not queue_arn:
+async def detect_changes(  # noqa: C901
+    config: Config, repo_dir: str
+) -> Union[str, None]:
+    if not config.sqs_queues:
         log.info("No queue arn found. Returning")
-        return False
+        return
 
-    queue_name = queue_arn.split(":")[-1]
-    region_name = queue_arn.split(":")[3]
-    session = await config.get_boto_session_from_arn(queue_arn, region_name)
-    identity = session.client("sts").get_caller_identity()
-    identity_arn_with_session_name = (
-        identity["Arn"].replace(":sts:", ":iam:").replace("assumed-role", "role")
-    )
-    # TODO: This only works for same account identities. We need to do similar to NoqMeter,
-    # check all roles we have access to on all accounts, or store this in configuration.
-    # Then exclude these from the list of roles to check.
+    role_messages = []
+    managed_policy_messages = []
+    permission_set_messages = []
+    commit_message = "Out of band changes detected.\nSummary:\n"
 
-    identity_arn = "/".join(identity_arn_with_session_name.split("/")[0:2])
-    sqs = session.client("sqs", region_name=region_name)
-    queue_url_res = sqs.get_queue_url(QueueName=queue_name)
-    queue_url = queue_url_res.get("QueueUrl")
-    messages = sqs.receive_message(QueueUrl=queue_url, MaxNumberOfMessages=10).get(
-        "Messages", []
-    )
+    for queue_arn in config.sqs_queues:
+        queue_name = queue_arn.split(":")[-1]
+        region_name = queue_arn.split(":")[3]
+        session = await config.get_boto_session_from_arn(queue_arn, region_name)
+        identity = session.client("sts").get_caller_identity()
+        identity_arn_with_session_name = (
+            identity["Arn"].replace(":sts:", ":iam:").replace("assumed-role", "role")
+        )
+        # TODO: This only works for same account identities. We need to do similar to NoqMeter,
+        # check all roles we have access to on all accounts, or store this in configuration.
+        # Then exclude these from the list of roles to check.
 
-    while messages:
-        processed_messages = []
-        for message in messages:
-            try:
-                processed_messages.append(
-                    {
-                        "Id": message["MessageId"],
-                        "ReceiptHandle": message["ReceiptHandle"],
-                    }
-                )
-                message_body = json.loads(message["Body"])
+        identity_arn = "/".join(identity_arn_with_session_name.split("/")[0:2])
+        sqs = session.client("sqs", region_name=region_name)
+        queue_url_res = sqs.get_queue_url(QueueName=queue_name)
+        queue_url = queue_url_res.get("QueueUrl")
+        messages = sqs.receive_message(QueueUrl=queue_url, MaxNumberOfMessages=10).get(
+            "Messages", []
+        )
+
+        while messages:
+            processed_messages = []
+            for message in messages:
                 try:
-                    if "Message" in message_body:
-                        decoded_message = json.loads(message_body["Message"])["detail"]
-                    else:
-                        decoded_message = message_body["detail"]
-                except Exception as err:
-                    log.debug(
-                        "Unable to process message", error=str(err), message=message
-                    )
                     processed_messages.append(
                         {
                             "Id": message["MessageId"],
                             "ReceiptHandle": message["ReceiptHandle"],
                         }
                     )
-                    continue
-                role_name = decoded_message["requestParameters"]["roleName"]
-                role_account_id = decoded_message.get(
-                    "account", decoded_message.get("recipientAccountId")
-                )
-                role_arn = f"arn:aws:iam::{role_account_id}:role/{role_name}"
-                actor = (
-                    decoded_message.get("userIdentity", {})
-                    .get("sessionContext", {})
-                    .get("sessionIssuer", {})
-                    .get("arn", "")
-                )
-                session_name = (
-                    decoded_message.get("userIdentity", {})
-                    .get("principalId")
-                    .split(":")[-1]
-                )
-                if actor != identity_arn:
-                    await send_iam_mutation_message(
-                        config=config,
-                        identity=role_arn,
-                        actor=actor,
-                        event_source=decoded_message["eventSource"],
-                        event_name=decoded_message["eventName"],
-                        session_name=session_name,
-                        cloudtrail_event=decoded_message,
+                    message_body = json.loads(message["Body"])
+                    try:
+                        if "Message" in message_body:
+                            decoded_message = json.loads(message_body["Message"])[
+                                "detail"
+                            ]
+                        else:
+                            decoded_message = message_body["detail"]
+                    except Exception as err:
+                        log.debug(
+                            "Unable to process message", error=str(err), message=message
+                        )
+                        processed_messages.append(
+                            {
+                                "Id": message["MessageId"],
+                                "ReceiptHandle": message["ReceiptHandle"],
+                            }
+                        )
+                        continue
+                    actor = (
+                        decoded_message.get("userIdentity", {})
+                        .get("sessionContext", {})
+                        .get("sessionIssuer", {})
+                        .get("arn", "")
                     )
-            except Exception as err:
-                log.debug("Unable to process message", error=str(err), message=message)
-                continue
+                    session_name = (
+                        decoded_message.get("userIdentity", {})
+                        .get("principalId")
+                        .split(":")[-1]
+                    )
+                    if actor != identity_arn:
+                        account_id = decoded_message.get("recipientAccountId")
+                        request_params = decoded_message["requestParameters"]
+                        event = decoded_message["eventName"]
+                        resource_id = None
+                        resource_type = None
+                        if role_name := request_params.get("roleName"):
+                            resource_id = role_name
+                            resource_type = "Role"
+                            role_messages.append(
+                                RoleMessageDetails(
+                                    account_id=account_id,
+                                    role_name=role_name,
+                                    delete=bool(event == "DeleteRole"),
+                                )
+                            )
+                        elif policy_arn := request_params.get("policyArn"):
+                            split_policy = policy_arn.split("/")
+                            policy_name = split_policy[-1]
+                            policy_path = (
+                                "/"
+                                if len(split_policy) == 2
+                                else f"/{'/'.join(split_policy[1:-1])}/"
+                            )
+                            resource_id = policy_name
+                            resource_type = "ManagedPolicy"
+                            managed_policy_messages.append(
+                                ManagedPolicyMessageDetails(
+                                    account_id=account_id,
+                                    policy_name=policy_name,
+                                    policy_path=policy_path,
+                                    delete=bool(
+                                        decoded_message["eventName"] == "DeletePolicy"
+                                    ),
+                                )
+                            )
+                        elif permission_set_arn := request_params.get(
+                            "permissionSetArn"
+                        ):
+                            resource_id = permission_set_arn
+                            resource_type = "PermissionSet"
+                            permission_set_messages.append(
+                                PermissionSetMessageDetails(
+                                    account_id=account_id,
+                                    instance_arn=request_params.get("instanceArn"),
+                                    permission_set_arn=permission_set_arn,
+                                )
+                            )
 
-        sqs.delete_message_batch(QueueUrl=queue_url, Entries=processed_messages)
-        messages = sqs.receive_message(QueueUrl=queue_url, MaxNumberOfMessages=10).get(
-            "Messages", []
+                        if resource_id:
+                            commit_message = (
+                                f"{commit_message}User {session_name} performed action {event} "
+                                f"on {resource_type}({resource_id}) on account {account_id}.\n"
+                            )
+                except Exception as err:
+                    log.debug(
+                        "Unable to process message", error=str(err), message=message
+                    )
+                    continue
+
+            sqs.delete_message_batch(QueueUrl=queue_url, Entries=processed_messages)
+            messages = sqs.receive_message(
+                QueueUrl=queue_url, MaxNumberOfMessages=10
+            ).get("Messages", [])
+
+    tasks = []
+    if role_messages:
+        tasks.append(generate_aws_role_templates([config], repo_dir, role_messages))
+    if managed_policy_messages:
+        tasks.append(
+            generate_aws_managed_policy_templates(
+                [config], repo_dir, managed_policy_messages
+            )
         )
-    return True
+    if permission_set_messages:
+        tasks.append(
+            generate_aws_permission_set_templates(
+                [config], repo_dir, permission_set_messages
+            )
+        )
+
+    if tasks:
+        await asyncio.gather(*tasks)
+        return commit_message
