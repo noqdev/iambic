60dd4d3baf60fd9f2bde54bf716dcabdb135f38e
Steven Moy <steven@noq.dev>
2023-01-12 16:58:25 -0800

    EN-1638 Address Permission Set Access Rules list sort stability

---
+++ test/aws/identity_center/test_template_generation.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+import pytest
+
+from iambic.aws.identity_center.permission_set.template_generation import (
+    _sorted_and_clean_access_rules,
+)
+
+
+@pytest.mark.parametrize(
+    "input,expected",
+    [
+        (
+            [
+                {
+                    "included_accounts": ["space in account name"],
+                    "included_orgs": ["xyz"],
+                    "users": ["bob@example.com"],
+                    "account_rule_key": "space in account name",
+                },
+                {
+                    "included_accounts": ["global_account_name"],
+                    "included_orgs": ["xyz"],
+                    "users": ["alice@example.com"],
+                    "account_rule_key": "global_account_name",
+                },
+            ],
+            [
+                {
+                    "included_accounts": ["global_account_name"],
+                    "included_orgs": ["xyz"],
+                    "users": ["alice@example.com"],
+                },
+                {
+                    "included_accounts": ["space in account name"],
+                    "included_orgs": ["xyz"],
+                    "users": ["bob@example.com"],
+                },
+            ],
+        )
+    ],
+)
+def test_sorted_and_clean_access_rules(input, expected):
+    assert _sorted_and_clean_access_rules(input) == expected
