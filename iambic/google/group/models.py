06e44f7534622d5a7606bde5a353afe4ff23e056
Steven Moy <steven@noq.dev>
2023-01-19 18:12:14 -0800

    Retry up to 5 times when get_group fails immediately after create_group

--- iambic/google/group/models.py
+++ iambic/google/group/models.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import asyncio
+import time
 from itertools import chain
 from typing import List, Optional
 
@@ -160,9 +161,16 @@ class GroupTemplate(GoogleTemplate, ExpiryModel):
                 description=self.properties.description,
                 google_project=google_project,
             )
-            current_group = await get_group(
-                self.properties.email, self.properties.domain, google_project
-            )
+            group_get_attempt = 0
+            while not current_group and group_get_attempt < 5:
+                # handle fetching group too fast
+                group_get_attempt = group_get_attempt + 1
+                current_group = await get_group(
+                    self.properties.email, self.properties.domain, google_project
+                )
+                if not current_group:
+                    time.sleep(1)
+
             if current_group:
                 change_details.current_value = current_group
 
