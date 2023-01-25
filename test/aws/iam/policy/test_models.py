6efac8fe6b5123822abbb7d1342c4bcd3c0af59b
Steven Moy <steven@noq.dev>
2023-01-25 13:03:05 -0800

    Handle when policy statement does not have sid and iambic has expires_at information

--- test/aws/iam/policy/test_models.py
+++ test/aws/iam/policy/test_models.py
@@ -4,7 +4,7 @@ from iambic.aws.iam.policy.models import PolicyDocument
 from iambic.core.models import merge_model
 
 
-def test_merge_policy_document():
+def test_merge_policy_document_with_sid():
     existing_statement_list = [
         {
             "effect": "Allow",
@@ -16,6 +16,27 @@ def test_merge_policy_document():
         policy_name="foo", version="bar", statement=existing_statement_list
     )
     new_statement_list = [{"effect": "Allow", "sid": "ExpireStatement"}]
+    new_document = PolicyDocument(
+        policy_name="foo", version="bar", statement=new_statement_list
+    )
+    merged_document: PolicyDocument = merge_model(existing_document, new_document)
+    assert (
+        merged_document.statement[0].expires_at
+        == existing_document.statement[0].expires_at
+    )
+
+
+def test_merge_policy_document_without_sid():
+    existing_statement_list = [
+        {
+            "effect": "Allow",
+            "expires_at": "2023-01-24",
+        }
+    ]
+    existing_document = PolicyDocument(
+        policy_name="foo", version="bar", statement=existing_statement_list
+    )
+    new_statement_list = [{"effect": "Allow"}]
     new_document = PolicyDocument(
         policy_name="foo", version="bar", statement=new_statement_list
     )
