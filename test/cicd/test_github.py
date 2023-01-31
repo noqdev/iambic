a18bd85628dae7bf7d5c2c10b1b99d84d6239c02
Steven Moy <steven@noq.dev>
2023-01-30 16:33:26 -0800

    EN-1675 Change pull-request flow to defer to issue-comment to handle plan request.

--- test/cicd/test_github.py
+++ test/cicd/test_github.py
@@ -26,7 +26,7 @@ def mock_github_client():
 
 
 @pytest.fixture
-def issue_comment_context():
+def issue_comment_git_apply_context():
     return {
         "server_url": "https://github.com",
         "run_id": "12345",
@@ -38,6 +38,30 @@ def issue_comment_context():
         "event": {
             "comment": {
                 "body": "iambic git-apply",
+            },
+            "issue": {
+                "number": 1,
+            },
+            "repository": {
+                "clone_url": "https://github.com/example-org/iambic-templates.git",
+            },
+        },
+    }
+
+
+@pytest.fixture
+def issue_comment_git_plan_context():
+    return {
+        "server_url": "https://github.com",
+        "run_id": "12345",
+        "run_attempt": "1",
+        "token": "fake-token",
+        "sha": "fake-sha",
+        "repository": "example.com/iambic-templates",
+        "event_name": "issue_comment",
+        "event": {
+            "comment": {
+                "body": "iambic git-plan",
             },
             "issue": {
                 "number": 1,
@@ -67,50 +91,58 @@ def mock_repository():
 
 
 def test_issue_comment_with_non_clean_mergeable_state(
-    mock_github_client, issue_comment_context, mock_lambda_run_handler
+    mock_github_client, issue_comment_git_apply_context, mock_lambda_run_handler
 ):
     mock_pull_request = mock_github_client.get_repo.return_value.get_pull.return_value
     mock_pull_request.mergeable_state = MERGEABLE_STATE_BLOCKED
-    handle_issue_comment(mock_github_client, issue_comment_context)
+    handle_issue_comment(mock_github_client, issue_comment_git_apply_context)
     assert mock_lambda_run_handler.called is False
     assert mock_pull_request.merge.called is False
 
 
 def test_issue_comment_with_not_applicable_comment_body(
-    mock_github_client, issue_comment_context, mock_lambda_run_handler
+    mock_github_client, issue_comment_git_apply_context, mock_lambda_run_handler
 ):
-    issue_comment_context["event"]["comment"]["body"] = "foo"
-    return_code = handle_issue_comment(mock_github_client, issue_comment_context)
+    issue_comment_git_apply_context["event"]["comment"]["body"] = "foo"
+    return_code = handle_issue_comment(
+        mock_github_client, issue_comment_git_apply_context
+    )
     assert return_code == HandleIssueCommentReturnCode.NO_MATCHING_BODY
 
 
 def test_issue_comment_with_clean_mergeable_state(
-    mock_github_client, issue_comment_context, mock_lambda_run_handler, mock_repository
+    mock_github_client,
+    issue_comment_git_apply_context,
+    mock_lambda_run_handler,
+    mock_repository,
 ):
     mock_pull_request = mock_github_client.get_repo.return_value.get_pull.return_value
     mock_pull_request.mergeable_state = MERGEABLE_STATE_CLEAN
-    mock_pull_request.head.sha = issue_comment_context["sha"]
-    mock_repository.clone_from.return_value.head.commit.hexsha = issue_comment_context[
-        "sha"
-    ]
-    handle_issue_comment(mock_github_client, issue_comment_context)
+    mock_pull_request.head.sha = issue_comment_git_apply_context["sha"]
+    mock_repository.clone_from.return_value.head.commit.hexsha = (
+        issue_comment_git_apply_context["sha"]
+    )
+    handle_issue_comment(mock_github_client, issue_comment_git_apply_context)
     assert mock_lambda_run_handler.called
     assert mock_pull_request.merge.called
 
 
 # invariant: PR is only merged if and only if git-apply is successful
 def test_issue_comment_with_clean_mergeable_state_and_lambda_handler_crashed(
-    mock_github_client, issue_comment_context, mock_lambda_run_handler, mock_repository
+    mock_github_client,
+    issue_comment_git_apply_context,
+    mock_lambda_run_handler,
+    mock_repository,
 ):
     mock_pull_request = mock_github_client.get_repo.return_value.get_pull.return_value
     mock_pull_request.mergeable_state = MERGEABLE_STATE_CLEAN
-    mock_pull_request.head.sha = issue_comment_context["sha"]
-    mock_repository.clone_from.return_value.head.commit.hexsha = issue_comment_context[
-        "sha"
-    ]
+    mock_pull_request.head.sha = issue_comment_git_apply_context["sha"]
+    mock_repository.clone_from.return_value.head.commit.hexsha = (
+        issue_comment_git_apply_context["sha"]
+    )
     mock_lambda_run_handler.side_effect = Exception("unexpected failure")
     with pytest.raises(Exception):
-        handle_issue_comment(mock_github_client, issue_comment_context)
+        handle_issue_comment(mock_github_client, issue_comment_git_apply_context)
     assert mock_lambda_run_handler.called
     assert not mock_pull_request.merge.called
 
@@ -157,6 +189,9 @@ def pull_request_context():
                 "clone_url": "https://github.com/example-org/iambic-templates.git",
             },
         },
+        "iambic": {
+            "GH_OVERRIDE_TOKEN": "fake_override_token",
+        },
     }
 
 
@@ -169,7 +204,12 @@ def test_pull_request_plan(
         "sha"
     ]
     handle_pull_request(mock_github_client, pull_request_context)
-    assert mock_lambda_run_handler.called is True
+    assert (
+        mock_lambda_run_handler.called is False
+    )  # because this flow only directly calls create_issue_comment on the pull request
+    assert (
+        not mock_pull_request.merge.called
+    )  # because this flow only issue the comment
 
 
 @pytest.mark.parametrize(
@@ -186,3 +226,20 @@ def test_pull_request_plan(
 def test_get_session_name(repo_name, pr_number, expected_result):
     session_name = get_session_name(repo_name, pr_number)
     assert session_name == expected_result
+
+
+def test_issue_comment_with_git_plan(
+    mock_github_client,
+    issue_comment_git_plan_context,
+    mock_lambda_run_handler,
+    mock_repository,
+):
+    mock_pull_request = mock_github_client.get_repo.return_value.get_pull.return_value
+    mock_pull_request.mergeable_state = MERGEABLE_STATE_CLEAN
+    mock_pull_request.head.sha = issue_comment_git_plan_context["sha"]
+    mock_repository.clone_from.return_value.head.commit.hexsha = (
+        issue_comment_git_plan_context["sha"]
+    )
+    handle_issue_comment(mock_github_client, issue_comment_git_plan_context)
+    assert mock_lambda_run_handler.called
+    assert not mock_pull_request.merge.called
