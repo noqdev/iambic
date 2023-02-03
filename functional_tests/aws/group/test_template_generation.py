cf395adeda85c137b0a5dcbd55e33c695090bc3b
Will-NOQ <102608210+Will-NOQ@users.noreply.github.com>
2023-02-03 08:00:15 -0600

    Added functional tests for IAM groups.

---
+++ functional_tests/aws/group/test_template_generation.py
@@ -0,0 +1,81 @@
+from __future__ import annotations
+
+import os
+from unittest import IsolatedAsyncioTestCase
+
+from functional_tests.aws.group.utils import generate_group_template_from_base
+from functional_tests.conftest import IAMBIC_TEST_DETAILS
+from iambic.aws.event_bridge.models import GroupMessageDetails
+from iambic.aws.iam.group.models import GroupTemplate
+from iambic.aws.iam.group.template_generation import generate_aws_group_templates
+from iambic.core.context import ctx
+
+
+class PartialImportGroupTestCase(IsolatedAsyncioTestCase):
+    async def asyncSetUp(self):
+        self.template = await generate_group_template_from_base(
+            IAMBIC_TEST_DETAILS.template_dir_path
+        )
+        self.group_name = self.template.properties.group_name
+        self.all_account_ids = [
+            account.account_id for account in IAMBIC_TEST_DETAILS.config.aws.accounts
+        ]
+
+    async def asyncTearDown(self):
+        self.template.deleted = True
+        await self.template.apply(IAMBIC_TEST_DETAILS.config, ctx)
+
+    async def test_delete_group_template(self):
+        included_account = self.all_account_ids[0]
+        self.template.included_accounts = [included_account]
+        self.template.excluded_accounts = []
+        self.template.write()
+
+        self.assertTrue(os.path.exists(self.template.file_path))
+
+        await generate_aws_group_templates(
+            [IAMBIC_TEST_DETAILS.config],
+            IAMBIC_TEST_DETAILS.template_dir_path,
+            [
+                GroupMessageDetails(
+                    account_id=included_account,
+                    group_name=self.template.properties.group_name,
+                    delete=True,
+                )
+            ],
+        )
+
+        self.assertFalse(os.path.exists(self.template.file_path))
+
+    async def test_delete_group_from_one_account(self):
+        deleted_account = self.all_account_ids[0]
+        self.template.included_accounts = ["*"]
+        self.template.excluded_accounts = []
+        self.template.write()
+
+        # Create on all accounts except 1
+        self.template.excluded_accounts = [deleted_account]
+
+        # Confirm the change is only in memory and not on the file system
+        file_sys_template = GroupTemplate.load(self.template.file_path)
+        self.assertNotIn(deleted_account, file_sys_template.excluded_accounts)
+
+        # Create the policy on all accounts except 1
+        await self.template.apply(IAMBIC_TEST_DETAILS.config, ctx)
+
+        # Refresh the template
+        await generate_aws_group_templates(
+            [IAMBIC_TEST_DETAILS.config],
+            IAMBIC_TEST_DETAILS.template_dir_path,
+            [
+                GroupMessageDetails(
+                    account_id=deleted_account,
+                    group_name=self.template.properties.group_name,
+                    delete=True,
+                )
+            ],
+        )
+
+        file_sys_template = GroupTemplate.load(self.template.file_path)
+        self.assertNotIn("*", file_sys_template.included_accounts)
+        self.assertNotIn(deleted_account, file_sys_template.included_accounts)
