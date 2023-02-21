43ca3b756b27ef2f40a86f59915204b3bbe8d40e
Steven Moy <steven@noq.dev>
2023-02-21 11:10:19 -0800

    Amend functional test to check relative time -> absolute time during apply

--- functional_tests/okta/group/test_okta_group.py
+++ functional_tests/okta/group/test_okta_group.py
@@ -92,9 +92,10 @@ properties:
     group_template.write()
 
     # Set expiry for the entire group
-    group_template.expires_at = datetime.datetime.now(
-        datetime.timezone.utc
-    ) - datetime.timedelta(days=1)
+    # group_template.expires_at = datetime.datetime.now(
+    #     datetime.timezone.utc
+    # ) - datetime.timedelta(days=1)
+    group_template.expires_at = "yesterday"
     group_template.write()
     run_apply(
         True,
@@ -105,3 +106,5 @@ properties:
 
     group_template = load_templates([test_group_fp])[0]
     assert group_template.deleted is True
+    assert group_template.expires_at != "yesterday"
+    assert isinstance(group_template.expires_at, datetime.datetime)
