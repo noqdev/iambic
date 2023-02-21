dfb010557dd7e65ee165b917c1547081be2b99ae
Steven Moy <steven@noq.dev>
2023-02-21 14:35:04 -0800

    Implement Github configs via Plugin interface

--- iambic/plugins/v0_1_0/example/handlers.py
+++ iambic/plugins/v0_1_0/example/handlers.py
@@ -3,12 +3,6 @@ from __future__ import annotations
 import asyncio
 from typing import TYPE_CHECKING
 
-# from iambic.plugins.v0_1_0.okta.app.template_generation import generate_app_templates
-# from iambic.plugins.v0_1_0.okta.group.template_generation import (
-#     generate_group_templates,
-# )
-# from iambic.plugins.v0_1_0.okta.user.template_generation import generate_user_templates
-
 if TYPE_CHECKING:
     from iambic.plugins.v0_1_0.example.iambic_plugin import ExampleConfig
 
