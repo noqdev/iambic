8592bef652b401a98d996799a770446721d2e20e
Matt Daue <mattdaue@gmail.com>
2023-02-27 14:35:56 -0800

    Ensure the shell selector works to write the correct alias
    Signed-off-by: Matt Daue <mattdaue@gmail.com>

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ then
     echo "Git is not installed on this system. Please install Git before running this script. Refer to your operating system's package manager for installation instructions."
 fi
 
-SHELL_NAME=$(ps -p $$ -o args= | awk '{print $1}')
+SHELL_NAME=$(ps -p $$ | tail -1 | awk '{print $NF}')
 echo "Detected shell: ${SHELL_NAME}"
 IAMBIC_GIT_REPO_PATH="${IAMBIC_GIT_REPO_PATH:-${HOME}/iambic_templates}"
 
@@ -21,31 +21,39 @@ CWD=$(pwd)
 cd ${IAMBIC_GIT_REPO_PATH}
 $(which git) init .
 cd $CWD
-DOCKER_ALIAS="alias iambic='docker run -it -u $(id -u):$(id -g) -v ${HOME}/.aws:/app/.aws -e AWS_CONFIG_FILE=/app/.aws/config -e AWS_SHARED_CREDENTIALS_FILE=/app/.aws/credentials -e AWS_PROFILE=${AWS_PROFILE} -v ${IAMBIC_GIT_REPO_PATH}:/templates:Z public.ecr.aws/s2p9s3r8/iambic:latest'"
+DOCKER_ALIAS="alias iambic='docker run -it -u $(id -u):$(id -g) -v ${HOME}/.aws:/app/.aws -e AWS_CONFIG_FILE=/app/.aws/config -e AWS_SHARED_CREDENTIALS_FILE=/app/.aws/credentials -e AWS_PROFILE=${AWS_PROFILE} -v ${CWD}:/templates:Z public.ecr.aws/s2p9s3r8/iambic:latest'"
 
 if [ "$SHELL_NAME" = "bash" ]; then
     echo "${DOCKER_ALIAS}" >> ~/.bashrc
+    echo "Wrote alias to ~/.bashrc"
     source ~/.bashrc
 elif [ "$SHELL_NAME" = "sh" ]; then
     echo "${DOCKER_ALIAS}" >> ~/.profile
+    echo "Wrote alias to ~/.profile"
     source ~/.profile
 elif [ "$SHELL_NAME" = "zsh" ]; then
     echo "${DOCKER_ALIAS}" >> ~/.zshrc
+    echo "Wrote alias to ~/.zshrc"
     source ~/.zshrc
 elif [ "$SHELL_NAME" = "ksh" ]; then
     echo "${DOCKER_ALIAS}" >> ~/.kshrc
+    echo "Wrote alias to ~/.kshrc"
     source ~/.kshrc
 elif [ "$SHELL_NAME" = "dash" ]; then
     echo "${DOCKER_ALIAS}" >> ~/.profile
+    echo "Wrote alias to ~/.profile"
     source ~/.profile
-elif [ "$SHELL_NAME" = "csh" ]; then
-    echo "${DOCKER_ALIAS}" >> ~/.cshrc
-    source ~/.cshrc
 elif [ "$SHELL_NAME" = "tcsh" ]; then
     echo "${DOCKER_ALIAS}" >> ~/.tcshrc
+    echo "Wrote alias to ~/.tcshrc"
     source ~/.tcshrc
+elif [ "$SHELL_NAME" = "csh" ]; then
+    echo "${DOCKER_ALIAS}" >> ~/.cshrc
+    echo "Wrote alias to ~/.cshrc"
+    source ~/.cshrc
 else
     echo "${DOCKER_ALIAS}" >> ~/.profile
+    echo "Wrote alias to ~/.profile"
     source ~/.profile
 fi
 
