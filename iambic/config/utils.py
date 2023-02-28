1169c20ce85129d0ea3d709ef0779072c032c1e0
Curtis Castrapel <curtis@noq.dev>
2023-02-28 13:34:13 -0800

    Huddle work

--- iambic/config/utils.py
+++ iambic/config/utils.py
@@ -2,10 +2,13 @@ from __future__ import annotations
 
 import pathlib
 import resource
-from iambic.config.dynamic_config import CoreConfig
+from typing import TYPE_CHECKING
 
 from iambic.core.logger import log
 from iambic.core.utils import gather_templates
+
+if TYPE_CHECKING:
+    from iambic.config.dynamic_config import Config
 
 
 async def resolve_config_template_path(repo_dir: str) -> pathlib.Path:
@@ -25,12 +28,19 @@ async def resolve_config_template_path(repo_dir: str) -> pathlib.Path:
     return pathlib.Path(config_template_file_path[0])
 
 
-def check_and_update_resource_limit():
+def check_and_update_resource_limit(config: Config):
     soft_limit, hard_limit = resource.getrlimit(resource.RLIMIT_NOFILE)
-    if soft_limit < CoreConfig.minimum_ulimit:
+    minimum_ulimit = 4096
+    if config.core:
+        minimum_ulimit = config.core.minimum_ulimit
+    if soft_limit < minimum_ulimit:
         try:
-            resource.setrlimit(resource.RLIMIT_NOFILE, (CoreConfig.minimum_ulimit, hard_limit))
+            resource.setrlimit(resource.RLIMIT_NOFILE, (minimum_ulimit, hard_limit))
         except PermissionError:
-            log.warning("Cannot increase resource limit: the process does not have permission.")
+            log.warning(
+                "Cannot increase resource limit: the process does not have permission."
+            )
         except Exception:
-            log.warning("Unable to increase resource limit: please manually update the soft limit to atleast 4096.")
+            log.warning(
+                "Unable to increase resource limit: please manually update the soft limit."
+            )
