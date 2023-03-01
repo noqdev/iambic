3ae9085025d6dfb1a40f065a31af54988e49d373
Matt Daue <mattdaue@gmail.com>
2023-03-01 11:51:19 -0800

    Remove git init (weird) and move iambic to /usr/local/bin, requires sudo now
    Signed-off-by: Matt Daue <mattdaue@gmail.com>

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 # Check if docker is installed
+echo "This script requires root privileges to install iambic in /usr/local/bin/; it also requires docker to be installed and running."
+
+if [[ id -u != 0 ]]; then
+    echo "Please run this script as root or with sudo. For example: curl https://iambic.org/install.sh | sudo bash"
+    exit
+fi
+
 if ! command -v docker &> /dev/null
 then
     echo "Docker is not installed on this system. Please install Docker before running this script. You can install docker by running the following command: curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh"
@@ -17,40 +24,33 @@ then
     echo "Git is not installed on this system. Please install Git before running this script. Refer to your operating system's package manager for installation instructions."
 fi
 
-if [[ -d ~/.local/bin ]]; then
-    echo "Detected ~/.local/bin directory, continuing..."
+if [[ -d /usr/local/bin ]]; then
+    echo "Detected /usr/local/bin directory, continuing..."
 else
-    echo "Creating ~/.local/bin directory..."
-    mkdir -p ~/.local/bin
+    echo "Creating /usr/local/bin directory..."
+    mkdir -p /usr/local/bin
 fi
 
-if echo $PATH | grep "\${HOME}/.local/bin" &> /dev/null; then
-    echo "Detected .local/bin is in the PATH, continuing..."
+if echo $PATH | grep "/usr/local/bin" &> /dev/null; then
+    echo "Detected /usr/local/bin is in the PATH, continuing..."
 else
-    echo "Please add the following line to your shell environment file: export PATH=\$PATH:\$HOME/.local/bin"
+    echo "Please add the following line to your shell environment file: export PATH=\$PATH:/usr/local/bin"
 fi
 
 echo
 
-IAMBIC_GIT_REPO_PATH="${IAMBIC_GIT_REPO_PATH:-${HOME}/iambic-templates}"
 IAMBIC_VERSION="${IAMBIC_VERSION:-latest}"
 ECR_PATH="public.ecr.aws/o4z3c2v2/iambic:latest"
 
 echo "Installing iambic..."
-echo "We are creating an iambic git repository in the directory ${IAMBIC_GIT_REPO_PATH}. If you want to change this directory, please edit the DEFAULT_IAMBIC_GIT_REPO variable in the install.sh script."
-mkdir -p ${IAMBIC_GIT_REPO_PATH}
-CWD=$(pwd)
-cd ${IAMBIC_GIT_REPO_PATH}
-$(which git) init .
-cd $CWD
 DOCKER_CMD="docker run -it -u \$(id -u):\$(id -g) -v \${HOME}/.aws:/app/.aws -e AWS_CONFIG_FILE=/app/.aws/config -e AWS_SHARED_CREDENTIALS_FILE=/app/.aws/credentials -e AWS_PROFILE=\${AWS_PROFILE} --mount \"type=bind,src=\$(pwd),dst=/templates\"  ${ECR_PATH} \"\$@\""
 
 echo
 
-echo "Setting up ~/.local/bin/iambic to launch the IAMbic docker container"
-echo "#!/bin/bash" > ~/.local/bin/iambic
-echo "${DOCKER_CMD}" >> ~/.local/bin/iambic
-chmod +x ~/.local/bin/iambic
+echo "Setting up /usr/local/bin/iambic to launch the IAMbic docker container"
+echo "#!/bin/bash" > /usr/local/bin/iambic
+echo "${DOCKER_CMD}" >> /usr/local/bin/iambic
+chmod +x /usr/local/bin/iambic
 
 echo "Caching the latest iambic docker container, this might take a minute"
 $( which docker ) pull ${ECR_PATH}
