9f8ad47e74ec848e415f609e2374e1e6be0066c4
Matt Daue <mattdaue@gmail.com>
2023-03-08 16:41:32 -0800

    Add unit test to test markdown generator and first jinja
    Signed-off-by: Matt Daue <mattdaue@gmail.com>

--- iambic/output/markdown.py
+++ iambic/output/markdown.py
@@ -1,7 +1,9 @@
+import pathlib
 from jinja2 import Environment, FileSystemLoader
 
 
 def render_resource_changes(resource_changes):
-    env = Environment(loader=FileSystemLoader('templates'))
+    my_path = pathlib.Path(__file__).parent.absolute()
+    env = Environment(loader=FileSystemLoader(my_path / 'templates'))
     template = env.get_template("resource_change.jinja2")
     return template.render(resource_changes=resource_changes)
