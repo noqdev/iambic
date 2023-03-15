324c1d4be12b03583874a68d7c28eb718d7607a0
Matt Daue <mattdaue@gmail.com>
2023-03-15 16:50:47 -0700

    Fix test
    Signed-off-by: Matt Daue <mattdaue@gmail.com>

--- test/config/test_dynamic_config.py
+++ test/config/test_dynamic_config.py
@@ -26,11 +26,12 @@ async def test_set_config_secrets_with_local_file(
 async def test_set_config_secrets_with_secret_in_secrets_manager(
     test_config, test_config_path_one_extends, secrets_setup
 ):
+    secret_key = "arn:aws:secretsmanager:us-west-2:123456789012:secret:iambic-config-secrets-9fae9066-5599-473f-b364-63fa0240b6f7"
     test_config.extends = [
         ExtendsConfig(
             key=ExtendsConfigKey.AWS_SECRETS_MANAGER,
-            value="arn:aws:secretsmanager:us-west-2:123456789012:secret:iambic-config-secrets-9fae9066-5599-473f-b364-63fa0240b6f7",
+            value=secret_key,
         )
     ]
     await test_config.set_config_secrets()
-    assert any(x.value == test_config_path_one_extends for x in test_config.extends)
+    assert any(x.value == secret_key for x in test_config.extends)
