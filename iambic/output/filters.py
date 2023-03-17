beb17e425b6a947b79e3314bb4e5a458fffa80f0
Matt Daue <mattdaue@gmail.com>
2023-03-17 14:01:20 -0700

    Fix tree view
    Signed-off-by: Matt Daue <mattdaue@gmail.com>

--- iambic/output/filters.py
+++ iambic/output/filters.py
@@ -1,3 +1,4 @@
+from io import StringIO
 from typing import List
 from colors import color
 from rich.console import Console    
@@ -30,35 +31,41 @@ def rich_text_table(table_headers: List[str], table_data: List[List[str]]) -> st
 
 
 def rich_tree_summary(action_summary: ActionSummary):
-    action_summary_tree = Tree(action_summary.action, expanded=False)
+    action_summary_tree = Tree(action_summary.action, expanded=True)
     for template in action_summary.templates:
-        template_tree = action_summary_tree.add(template.template_path, expanded=False)
+        template_tree = action_summary_tree.add(template.template_path, expanded=True)
         for account in template.accounts:
-            account_tree = template_tree.add(account.account, expanded=False)
+            account_tree = template_tree.add(account.account, expanded=True)
             for change in account.changes:
-                change_tree = account_tree.add(change.change.change_type, expanded=False)
-                change_tree.add(change.resource_id)
-                change_tree.add(change.resource_type)
-                change_tree.add(change.change.change_type.value)
+                change_tree = account_tree.add(str(change.change.change_type), expanded=True)
+                change_tree.add(change.change.resource_id)
+                change_tree.add(change.change.resource_type)
+                change_tree.add(str(change.change.change_type.value))
                 if change.change.diff:
                     change_tree.add(change.change.diff)
-    return action_summary_tree
+    console = Console(file=StringIO(), force_terminal=True)
+    console.print(action_summary_tree)
+    output = console.file.getvalue()
+    return output
 
 
 def rich_tree_exception(exceptions: ExceptionSummary):
-    exception_tree = Tree(exceptions.action, expanded=False)
+    exception_tree = Tree(exceptions.action, expanded=True)
     for template in exceptions.templates:
-        template_tree = exception_tree.add(template.template_path, expanded=False)
+        template_tree = exception_tree.add(template.template_path, expanded=True)
         for account in template.accounts:
-            account_tree = template_tree.add(account.account, expanded=False)
+            account_tree = template_tree.add(account.account, expanded=True)
             for change in account.changes:
-                change_tree = account_tree.add(change.change.change_type, expanded=False)
-                change_tree.add(change.resource_id)
-                change_tree.add(change.resource_type)
-                change_tree.add(change.change.change_type.value)
+                change_tree = account_tree.add(str(change.change.change_type), expanded=True)
+                change_tree.add(change.change.resource_id)
+                change_tree.add(change.change.resource_type)
+                change_tree.add(str(change.change.change_type.value))
                 if change.change.diff:
                     change_tree.add(change.change.diff)
-    return exception_tree
+    console = Console(file=StringIO(), force_terminal=True)
+    console.print(exception_tree)
+    output = console.file.getvalue()
+    return output
 
 
 def ansi_format(value, style_str):
