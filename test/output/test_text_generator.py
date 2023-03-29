0abc827a0fcfa392bcd99110ab4a7ad8da20b1ad
Matt Daue <mattdaue@gmail.com>
2023-03-29 13:28:27 -0700

    Update tests to remove real mock account num
    Signed-off-by: Matt Daue <mattdaue@gmail.com>

--- test/output/test_text_generator.py
+++ test/output/test_text_generator.py
@@ -16,11 +16,11 @@ from . import get_update_template, get_templates_mixed
             get_templates_mixed(),
             [
                 "aws:iam:role // product-dev_iambic_test_role",
-                "design-prod - (006933239187)",
-                "Iambic Standalone Org - (566255053759)",
+                "design-prod - (234567890123)",
+                "Iambic Standalone Org - (456789012356)",
                 "aws:iam:role // design-dev_iambic_test_role",
                 "aws:iam:role // design-workspaces_iambic_test_role",
-                "design-prod - (006933239187)",
+                "design-prod - (234567890123)",
             ]
         ),
         (
@@ -49,11 +49,11 @@ def test_screen_render_resource_changes(
             get_templates_mixed(),
             [
                 "aws:iam:role // product-dev_iambic_test_role",
-                "design-prod - (006933239187)",
-                "Iambic Standalone Org - (566255053759)",
+                "design-prod - (234567890123)",
+                "Iambic Standalone Org - (456789012356)",
                 "aws:iam:role // design-dev_iambic_test_role",
                 "aws:iam:role // design-workspaces_iambic_test_role",
-                "design-prod - (006933239187)",
+                "design-prod - (234567890123)",
             ]
         ),
         (
