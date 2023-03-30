66b89b7001bfa3e932b3e0d52193590902d7584d
Steven Moy <steven@noq.dev>
2023-03-30 11:16:51 -0700

    Attempt to make test faster

--- functional_tests/aws/permission_set/test_update_template.py
+++ functional_tests/aws/permission_set/test_update_template.py
@@ -9,6 +9,7 @@ from functional_tests.aws.permission_set.utils import (
 )
 from functional_tests.conftest import IAMBIC_TEST_DETAILS
 from iambic.core import noq_json as json
+from iambic.core.context import ctx
 from iambic.output.text import screen_render_resource_changes
 from iambic.plugins.v0_1_0.aws.identity_center.permission_set.models import (
     PermissionSetAccess,
@@ -26,12 +27,14 @@ EXAMPLE_USER = (
 class UpdatePermissionSetTestCase(IsolatedAsyncioTestCase):
     @classmethod
     def setUpClass(cls):
+        ctx.eval_only = False
         cls.template = asyncio.run(
             generate_permission_set_template_from_base(
                 IAMBIC_TEST_DETAILS.template_dir_path
             )
         )
         asyncio.run(cls.template.apply(IAMBIC_TEST_DETAILS.config.aws))
+        cls.permission_set_arn = None
         sleep(5)
         asyncio.run(
             IAMBIC_TEST_DETAILS.identity_center_account.set_identity_center_details(
@@ -41,31 +44,36 @@ class UpdatePermissionSetTestCase(IsolatedAsyncioTestCase):
 
     @classmethod
     def tearDownClass(cls):
-        sleep(5)
         cls.template.deleted = True
         asyncio.run(cls.template.apply(IAMBIC_TEST_DETAILS.config.aws))
-        sleep(5)
-        asyncio.run(
-            IAMBIC_TEST_DETAILS.identity_center_account.set_identity_center_details(
-                batch_size=5
-            )
-        )
 
-    async def test_update_description(self):
-        self.template.properties.description = "Updated description"
+    async def test_update_valid_description(self):
+        updated_description = "Updated description"
+        assert self.template.properties.description != updated_description
+        self.template.properties.description = updated_description
         changes = await self.template.apply(IAMBIC_TEST_DETAILS.config.aws)
         screen_render_resource_changes([changes])
-        await IAMBIC_TEST_DETAILS.identity_center_account.set_identity_center_details(
-            batch_size=5
+
+        identity_center_client = await IAMBIC_TEST_DETAILS.identity_center_account.get_boto3_client(
+            "sso-admin",
+            region_name=IAMBIC_TEST_DETAILS.identity_center_account.identity_center_details.region_name,
+        )
+        sso_admin_instance_arn = (
+            IAMBIC_TEST_DETAILS.identity_center_account.identity_center_details.instance_arn
+        )
+        permission_set_arn = IAMBIC_TEST_DETAILS.identity_center_account.identity_center_details.permission_set_map[
+            self.template.identifier
+        ][
+            "PermissionSetArn"
+        ]
+        response = identity_center_client.describe_permission_set(
+            InstanceArn=sso_admin_instance_arn,
+            PermissionSetArn=permission_set_arn,
         )
 
         self.assertEqual(
             self.template.properties.description,
-            IAMBIC_TEST_DETAILS.identity_center_account.identity_center_details.permission_set_map[
-                self.template.identifier
-            ][
-                "Description"
-            ],
+            response["PermissionSet"]["Description"],
         )
 
     async def test_account_assignment(self):
