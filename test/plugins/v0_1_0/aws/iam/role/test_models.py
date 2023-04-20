9a37e663c650a9470202561bf9731b51aeaca3b8
Will-NOQ <102608210+Will-NOQ@users.noreply.github.com>
2023-04-20 10:53:02 -0500

    Updated docs to account for var prefix in templates.

--- test/plugins/v0_1_0/aws/iam/role/test_models.py
+++ test/plugins/v0_1_0/aws/iam/role/test_models.py
@@ -21,7 +21,7 @@ def test_merge_role_template_without_sid(aws_accounts: list[AWSAccount]):
         ],
     }
     existing_document = AwsIamRoleTemplate(
-        identifier="{{account_name}}_iambic_test_role",
+        identifier="{{var.account_name}}_iambic_test_role",
         file_path="foo",
         properties=existing_properties,
     )
@@ -39,7 +39,7 @@ def test_merge_role_template_without_sid(aws_accounts: list[AWSAccount]):
         ],
     }
     new_document = AwsIamRoleTemplate(
-        identifier="{{account_name}}_iambic_test_role",
+        identifier="{{var.account_name}}_iambic_test_role",
         file_path="foo",
         properties=new_properties,
     )
@@ -80,7 +80,7 @@ def test_merge_role_template_access_rules(aws_accounts: list[AWSAccount]):
         }
     ]
     existing_document = AwsIamRoleTemplate(
-        identifier="{{account_name}}_iambic_test_role",
+        identifier="{{var.account_name}}_iambic_test_role",
         file_path="foo",
         properties=existing_properties,
         access_rules=existing_access_rules,
@@ -106,7 +106,7 @@ def test_merge_role_template_access_rules(aws_accounts: list[AWSAccount]):
         }
     ]
     new_document = AwsIamRoleTemplate(
-        identifier="{{account_name}}_iambic_test_role",
+        identifier="{{var.account_name}}_iambic_test_role",
         file_path="foo",
         properties=new_properties,
         access_rules=new_access_rules,
@@ -155,7 +155,7 @@ def test_merge_role_with_forked_policy(aws_accounts: list[AWSAccount]):
         ],
     }
     existing_document = AwsIamRoleTemplate(
-        identifier="{{account_name}}_iambic_test_role",
+        identifier="{{var.account_name}}_iambic_test_role",
         file_path="foo",
         properties=existing_properties,
     )
@@ -188,7 +188,7 @@ def test_merge_role_with_forked_policy(aws_accounts: list[AWSAccount]):
         ],
     }
     new_document = AwsIamRoleTemplate(
-        identifier="{{account_name}}_iambic_test_role",
+        identifier="{{var.account_name}}_iambic_test_role",
         file_path="foo",
         properties=new_properties,
     )
@@ -249,7 +249,7 @@ def test_merge_role_with_access_preservation(aws_accounts: list[AWSAccount]):
         ],
     }
     existing_document = AwsIamRoleTemplate(
-        identifier="{{account_name}}_iambic_test_role",
+        identifier="{{var.account_name}}_iambic_test_role",
         file_path="foo",
         properties=existing_properties,
     )
@@ -289,7 +289,7 @@ def test_merge_role_with_access_preservation(aws_accounts: list[AWSAccount]):
             non_prod_expires_at = inline_policy.expires_at
 
     new_document = AwsIamRoleTemplate(
-        identifier="{{account_name}}_iambic_test_role",
+        identifier="{{var.account_name}}_iambic_test_role",
         file_path="foo",
         properties=new_properties,
     )
@@ -330,7 +330,7 @@ def test_merge_role_with_assignment_resolution(aws_accounts: list[AWSAccount]):
         ],
     }
     existing_document = AwsIamRoleTemplate(
-        identifier="{{account_name}}_iambic_test_role",
+        identifier="{{var.account_name}}_iambic_test_role",
         file_path="foo",
         properties=existing_properties,
     )
@@ -351,7 +351,7 @@ def test_merge_role_with_assignment_resolution(aws_accounts: list[AWSAccount]):
         ],
     }
     new_document = AwsIamRoleTemplate(
-        identifier="{{account_name}}_iambic_test_role",
+        identifier="{{var.account_name}}_iambic_test_role",
         file_path="foo",
         properties=new_properties,
     )
@@ -394,7 +394,7 @@ def test_merge_role_with_new_excluded_account(aws_accounts: list[AWSAccount]):
         ],
     }
     existing_document = AwsIamRoleTemplate(
-        identifier="{{account_name}}_iambic_test_role",
+        identifier="{{var.account_name}}_iambic_test_role",
         file_path="foo",
         properties=existing_properties,
     )
@@ -415,7 +415,7 @@ def test_merge_role_with_new_excluded_account(aws_accounts: list[AWSAccount]):
         ],
     }
     new_document = AwsIamRoleTemplate(
-        identifier="{{account_name}}_iambic_test_role",
+        identifier="{{var.account_name}}_iambic_test_role",
         file_path="foo",
         properties=new_properties,
     )
@@ -468,7 +468,7 @@ def test_merge_role_with_multiple_access_removals(aws_accounts: list[AWSAccount]
         ],
     }
     existing_document = AwsIamRoleTemplate(
-        identifier="{{account_name}}_iambic_test_role",
+        identifier="{{var.account_name}}_iambic_test_role",
         file_path="foo",
         properties=existing_properties,
     )
@@ -488,7 +488,7 @@ def test_merge_role_with_multiple_access_removals(aws_accounts: list[AWSAccount]
         ],
     }
     new_document = AwsIamRoleTemplate(
-        identifier="{{account_name}}_iambic_test_role",
+        identifier="{{var.account_name}}_iambic_test_role",
         file_path="foo",
         properties=new_properties,
     )
