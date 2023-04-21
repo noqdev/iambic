fa71089c109b65aecf143e8687981011883e5c07
Jonathan Loscalzo <loscalzo.jony@gmail.com>
2023-04-21 11:56:09 -0300

    add test  to check transitions

--- test/plugins/v0_1_0/okta/user/test_utils.py
+++ test/plugins/v0_1_0/okta/user/test_utils.py
@@ -7,6 +7,7 @@ from test.plugins.v0_1_0.okta.test_utils import (  # noqa: F401 # intentional fo
 import okta.models
 import pytest
 
+from iambic.core.context import ctx
 import iambic.plugins.v0_1_0.okta.models
 from iambic.core.models import ProposedChangeType
 from iambic.plugins.v0_1_0.okta.iambic_plugin import OktaOrganization
@@ -17,7 +18,9 @@ from iambic.plugins.v0_1_0.okta.user.utils import (
     get_user,
     maybe_deprovision_user,
     update_user_profile,
+    update_user_status,
 )
+from iambic.plugins.v0_1_0.okta.models import UserStatus
 
 
 @pytest.mark.asyncio
@@ -152,6 +155,66 @@ async def test_update_user_profile(
         "current_profile": input_user.profile,
         "new_profile": input_profile,
     }
+
+
+class TestUpdateUserStatus:
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize(
+        "transition",
+        [
+            (UserStatus.provisioned, UserStatus.deprovisioned),
+            (UserStatus.active, UserStatus.deprovisioned),
+            (UserStatus.suspended, UserStatus.active),
+            (UserStatus.active, UserStatus.suspended),
+            (UserStatus.staged, UserStatus.active),
+            (UserStatus.deprovisioned, UserStatus.active),
+            (UserStatus.staged, UserStatus.provisioned),
+            (UserStatus.deprovisioned, UserStatus.provisioned),
+            (UserStatus.provisioned, UserStatus.active),
+            (UserStatus.locked_out, UserStatus.active),
+        ],
+    )
+    async def test_update_user_status(
+        self,
+        mock_okta_organization: OktaOrganization,  # noqa: F811 # intentional for mocks
+        mock_ctx,
+        transition,
+    ):
+        # Have to create user before getting it
+        username = "example_username"
+        idp_name = "example.org"
+        user_properties = UserProperties(
+            username=username,
+            profile={"login": username},
+            status=transition[0].value,
+        )  # type: ignore
+        template = OktaUserTemplate(
+            file_path="example", idp_name=idp_name, properties=user_properties
+        )  # type: ignore
+
+        okta_user = await create_user(
+            template,
+            mock_okta_organization,
+        )
+        okta_user.status = transition[0]
+
+        mock_ctx(eval_only=True)
+        proposed_changes = await update_user_status(
+            okta_user,
+            transition[1].value,
+            mock_okta_organization,
+            {},
+        )
+
+        print(proposed_changes)
+        assert proposed_changes[0].change_type == ProposedChangeType.UPDATE
+        assert proposed_changes[0].resource_type == okta_user.resource_type
+        assert proposed_changes[0].resource_id == okta_user.user_id
+        assert proposed_changes[0].attribute == "status"
+        assert proposed_changes[0].change_summary == {
+            "current_status": transition[0].value,
+            "proposed_status": transition[1].value,
+        }
 
 
 @pytest.mark.asyncio
