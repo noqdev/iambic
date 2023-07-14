b8b1d855b90d9532cffcd92f9b65e93ea20e0fed
Steven Moy <steven@noq.dev>
2023-07-14 13:38:15 -0700

    Validate google response data

--- iambic/plugins/v0_1_0/google_workspace/group/utils.py
+++ iambic/plugins/v0_1_0/google_workspace/group/utils.py
@@ -328,6 +328,15 @@ async def get_group_members(service, group):
     http = _auth.authorized_http(service._http.credentials)
     member_req = service.members().list(groupKey=group["email"])
     member_res = member_req.execute(http=http) or {}
+    members = member_res.get("members", [])
+    required_keys = ["email", "role", "type"]
+    # validate response data because we have reports that member without email address
+    for member in members:
+        missing_required_keys = set(required_keys) - set(member.keys())
+        if len(missing_required_keys) > 0:
+            raise ValueError(
+                f"missing keys: {missing_required_keys} for member: {member}"
+            )
     return [
         GroupMember(
             email=member["email"],
@@ -335,7 +344,7 @@ async def get_group_members(service, group):
             type=GroupMemberType(member["type"]),
             status=GroupMemberStatus(member.get("status", GroupMemberStatus.UNDEFINED)),
         )
-        for member in member_res.get("members", [])
+        for member in members
     ]
 
 
