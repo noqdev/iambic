c96bf9c5ef42aa87a536c5d869f5b0e9e6a34183
Steven Moy <steven@noq.dev>
2023-07-26 14:34:36 -0700

    Implement switching between lambda function url vs API Gateway

--- deployment/github_app/output.tf
+++ deployment/github_app/output.tf
@@ -1,3 +1,7 @@
 output "function_url" {
-  value = aws_lambda_function_url.iambic_github_app.function_url
+  value = aws_lambda_function_url.iambic_github_app[*].function_url
+}
+
+output "api_gateway_url" {
+  value = aws_api_gateway_stage.prod[*].invoke_url
 }
