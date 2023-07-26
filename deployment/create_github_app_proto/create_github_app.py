993d2da13c692e3e307270e5692aa37e1b1a2d13
Steven Moy <steven@noq.dev>
2023-07-25 17:15:52 -0700

    Prototype GitHub App creation flow

---
+++ deployment/create_github_app_proto/create_github_app.py
@@ -0,0 +1,47 @@
+# https://docs.github.com/en/apps/sharing-github-apps/registering-a-github-app-from-a-manifest#examples
+
+from __future__ import annotations
+
+import aiohttp
+import yaml
+from aiohttp import web
+
+routes = web.RouteTableDef()
+
+# @routes.get('/')
+# async def hello(request):
+#     return web.Response(text="Hello, world")
+
+
+@routes.get("/")
+async def index(request):
+    return web.FileResponse("./index.html")
+
+
+@routes.get("/redirect")
+async def redirect(request):
+    ## here how to get query parameters
+    code = request.rel_url.query["code"]
+    state = request.rel_url.query["state"]
+    result = f"code: {code}, state: {state}"
+    print(result)
+    await exchange_token(code)
+    return web.Response(text="ok")
+
+
+async def exchange_token(code):
+    async with aiohttp.ClientSession("https://api.github.com/") as session:
+        async with session.post(f"/app-manifests/{code}/conversions") as resp:
+            print(resp.status)
+            payload = await resp.json()
+    dot_env = {
+        "webhook_secret": payload["webhook_secret"],
+        "pem": payload["pem"],
+    }
+    with open(".env", "w") as f:
+        yaml.dump(dot_env, f)
+
+
+app = web.Application()
+app.add_routes(routes)
+web.run_app(app)
