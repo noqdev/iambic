c8715496e80c987e828ac51d2c2b6e717e311300
Steven Moy <steven@noq.dev>
2023-07-25 20:01:55 -0700

    Terraform module to setup ECR pull through cache

---
+++ deployment/iambic_ecr/vars.tf
@@ -0,0 +1,35 @@
+variable "aws_region" {
+  type        = string
+  description = "The aws region in which the lambda function will be deployed"
+
+  default = "us-west-2"
+}
+
+variable "manage_aws_ecr_pull_through_cache" {
+  type = bool
+
+  # Pull through cache rule may already exist in AWS account
+  # If that's the case, you can change the value to false.
+  description = "Have this terraform module manage pull through cache"
+
+  default = true
+}
+
+variable "ecr_repository_prefix" {
+  type        = string
+  description = "Prefix to reach public ecr"
+
+  default = "ecr-public"
+}
+
+variable "ecr_public_alias" {
+  type        = string
+  description = "alias from ECR public"
+  default = "iambic"
+}
+
+variable "public_repo_name" {
+  type        = string
+  description = "Repo name under the ECR alias"
+  default = "iambic"
+}
