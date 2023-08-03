97108e41a288f3f7b9fd5edf1d7b6dc1a2de83be
Curtis <ccastrapel@gmail.com>
2023-08-03 12:27:08 -0700

    Forgot to push - date format

--- iambic/core/noq_json.py
+++ iambic/core/noq_json.py
@@ -19,7 +19,7 @@ class SetEncoder(json.JSONEncoder):
         if isinstance(obj, datetime):
             return obj.strftime("%Y-%m-%d %H:%M:%S %Z")
         if isinstance(obj, date):
-            return obj.strftime("%Y-%m-%d %H:%M:%S %Z")
+            return obj.strftime("%Y-%m-%d %H:%M:%S")
         if isinstance(obj, Exception):
             return str(obj)
         if isinstance(obj, UUID):
