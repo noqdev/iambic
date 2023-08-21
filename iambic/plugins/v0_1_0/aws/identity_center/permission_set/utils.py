9e3b0b345a47369443708844b2be0c5b1f6d1368
Steven Moy <steven@noq.dev>
2023-08-21 15:13:02 -0700

    Read an environment variable to skip a non-resolvable PrincipalID

--- iambic/plugins/v0_1_0/aws/identity_center/permission_set/utils.py
+++ iambic/plugins/v0_1_0/aws/identity_center/permission_set/utils.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import asyncio
+import os
 from functools import cache
 from itertools import chain
 
@@ -20,6 +21,14 @@ from iambic.plugins.v0_1_0.aws.utils import boto_crud_call, legacy_paginated_sea
 # IdentityCenter may reach a state we cannot resolve
 # the PrincipalID from the directory.
 IS_IAMBIC_FALLBACK_VALUE = "IS_IAMBIC_FALLBACK_VALUE"
+
+
+# Why are we relying on os.environ instead of config?
+# Because PrincipalID resolution function does not
+# have the config object available.
+IAMBIC_SKIP_NOT_RESOLVABLE_PRINCIPAL_ID = os.environ.get(
+    "IAMBIC_SKIP_NOT_RESOLVABLE_PRINCIPAL_ID", False
+)
 
 
 async def get_permission_set_details(
@@ -180,6 +189,9 @@ async def get_permission_set_users_and_groups(
                         log.error(
                             f"permission set: {permission_set_arn} cannot resolve user PrincipalID: {principal_id} for account: {account_id}"
                         )
+                        if IAMBIC_SKIP_NOT_RESOLVABLE_PRINCIPAL_ID:
+                            # skip the fallback value if we were told to skip it.
+                            continue
                     user_name = info.get("UserName", "ERROR_UNABLE_TO_LOOKUP_USERNAME")
                     display_name = info.get(
                         "DisplayName", "ERROR_UNABLE_TO_LOOKUP_DISPLAYNAME"
@@ -198,6 +210,10 @@ async def get_permission_set_users_and_groups(
                         log.error(
                             f"permission set: {permission_set_arn} cannot resolve group PrincipalID: {principal_id} for account: {account_id}"
                         )
+                        if IAMBIC_SKIP_NOT_RESOLVABLE_PRINCIPAL_ID:
+                            # skip the fallback value if we were told to skip it.
+                            continue
+
                     group_id = info.get("GroupId", "ERROR_UNABLE_TO_LOOKUP_GROUPID")
                     display_name = info.get(
                         "DisplayName", "ERROR_UNABLE_TO_LOOKUP_DISPLAYNAME"
@@ -212,9 +228,14 @@ async def get_permission_set_users_and_groups(
             # End Special case handling when identity_store using AD integrations
             # cannot list users and groups ahead of time without filters.
 
-            response[aa["PrincipalType"].lower()][aa["PrincipalId"]]["accounts"].append(
-                aa["AccountId"]
-            )
+            if aa["PrincipalId"] in response[aa["PrincipalType"].lower()]:
+                # the conditional can be false when the data in the control
+                # plane is in inconsistent state. Meaning. IdentityCenter account
+                # assignment exists when it's not possible to identify user.
+                # yes... it can happen
+                response[aa["PrincipalType"].lower()][aa["PrincipalId"]][
+                    "accounts"
+                ].append(aa["AccountId"])
 
     response["user"] = {k: v for k, v in response["user"].items() if v["accounts"]}
     response["group"] = {k: v for k, v in response["group"].items() if v["accounts"]}
