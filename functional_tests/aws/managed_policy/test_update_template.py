cbffe724d4ec8d0afacf21ddb333b4b74d5643d4
Steven Moy <steven@noq.dev>
2023-09-01 15:38:16 -0700

    move class setup to async setup to help with flaky test

--- functional_tests/aws/managed_policy/test_update_template.py
+++ functional_tests/aws/managed_policy/test_update_template.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-import asyncio
+import os
 from unittest import IsolatedAsyncioTestCase
 
 from functional_tests.aws.managed_policy.utils import (
@@ -13,27 +13,20 @@ from iambic.plugins.v0_1_0.aws.models import Tag
 
 
 class ManagedPolicyUpdateTestCase(IsolatedAsyncioTestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.template = asyncio.run(
-            generate_managed_policy_template_from_base(
-                IAMBIC_TEST_DETAILS.template_dir_path
-            )
+    async def asyncSetUp(self):
+        self.template = await generate_managed_policy_template_from_base(
+            IAMBIC_TEST_DETAILS.template_dir_path
         )
-        cls.all_account_ids = [
+        self.all_account_ids = [
             account.account_id for account in IAMBIC_TEST_DETAILS.config.aws.accounts
         ]
-        # Only include the template in half the accounts
-        # Make the accounts explicit so it's easier to validate account scoped tests
-        cls.template.included_accounts = cls.all_account_ids[
-            : len(cls.all_account_ids) // 2
-        ]
-        asyncio.run(cls.template.apply(IAMBIC_TEST_DETAILS.config.aws))
+        # only test the first two accounts for speed
+        self.template.included_accounts = self.all_account_ids[0:2]
 
-    @classmethod
-    def tearDownClass(cls):
-        cls.template.deleted = True
-        asyncio.run(cls.template.apply(IAMBIC_TEST_DETAILS.config.aws))
+    async def asyncTearDown(self):
+        if os.path.exists(self.template.file_path):
+            self.template.deleted = True
+            await self.template.apply(IAMBIC_TEST_DETAILS.config.aws)
 
     # tag None string value is not acceptable
     async def test_update_tag_with_bad_input(self):
