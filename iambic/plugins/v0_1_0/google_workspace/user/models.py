ff2e91554e654c16eae82f2b8a34623bf6373de3
Curtis <ccastrapel@gmail.com>
2023-09-25 11:16:25 -0700

    WIP

--- iambic/plugins/v0_1_0/google_workspace/user/models.py
+++ iambic/plugins/v0_1_0/google_workspace/user/models.py
@@ -1,11 +1,26 @@
 from __future__ import annotations
 
+from asyncio import gather
+from itertools import chain
 from typing import TYPE_CHECKING, Optional
 
 from pydantic import Field
 
-from iambic.core.iambic_enum import IambicManaged
-from iambic.core.models import BaseModel, BaseTemplate, ExpiryModel
+from iambic.core.context import ctx
+from iambic.core.iambic_enum import Command, IambicManaged
+from iambic.core.logger import log
+from iambic.core.models import (
+    AccountChangeDetails,
+    BaseModel,
+    BaseTemplate,
+    ExpiryModel,
+)
+from iambic.plugins.v0_1_0.google_workspace.user.utils import (
+    get_user,
+    maybe_delete_user,
+    update_user_email,
+    update_user_name,
+)
 
 if TYPE_CHECKING:
     from iambic.plugins.v0_1_0.google_workspace.iambic_plugin import GoogleProject
@@ -66,6 +81,96 @@ class GoogleWorkspaceUserTemplate(BaseTemplate, ExpiryModel):
     # owner metadata seems strange for a user (maybe it makes more sense if its machine user)
     # owner: Optional[str] = Field(None, description="Owner of the group")
     properties: WorkspaceUser
+
+    async def _apply_to_account(
+        self, google_project: GoogleProject
+    ) -> AccountChangeDetails:
+        proposed_user = {
+            # Populate user properties here, similar to apply_resource_dict
+            "primaryEmail": self.properties.primary_email,
+            "name": self.properties.name,
+            # ... other properties
+        }
+
+        change_details = AccountChangeDetails(
+            account=self.properties.domain,
+            resource_id=self.properties.primary_email,
+            resource_type=self.resource_type,
+            new_value=proposed_user,
+            proposed_changes=[],
+        )
+
+        log_params = {
+            "resource_type": self.resource_type,
+            "resource_id": self.properties.primary_email,
+            "account": str(self.properties.domain),
+        }
+
+        current_user = await get_user(
+            self.properties.primary_email, self.properties.domain, google_project
+        )
+
+        if current_user:
+            change_details.current_value = current_user
+            if ctx.command == Command.CONFIG_DISCOVERY:
+                change_details.new_value = {}
+                return change_details
+
+        user_exists = bool(current_user)
+
+        tasks = []
+
+        await self.remove_expired_resources()
+
+        if not user_exists:
+            if self.deleted:
+                log.info(
+                    "Resource is marked for deletion but does not exist in the cloud. Skipping."
+                )
+                return change_details
+            # Handle user creation here
+            await create_user(
+                primary_email=self.properties.primary_email,
+                name=self.properties.name,
+                domain=self.properties.domain,
+                google_project=google_project,
+            )
+
+        tasks.extend(
+            [
+                update_user_email(
+                    current_user.properties.email,
+                    self.properties.primary_email,
+                    log_params,
+                ),
+                update_user_name(
+                    current_user.properties.name, self.properties.name, log_params
+                ),
+                # Add more update functions for other user properties
+                maybe_delete_user(self, google_project, log_params),
+            ]
+        )
+
+        changes_made = await gather(*tasks)
+        if any(changes_made):
+            change_details.extend_changes(list(chain.from_iterable(changes_made)))
+
+        if ctx.execute:
+            log.debug(
+                "Successfully finished execution for resource",
+                changes_made=bool(change_details.proposed_changes),
+                **log_params,
+            )
+            if self.deleted:
+                self.delete()
+            self.write()
+        else:
+            log.debug(
+                "Successfully finished scanning for drift for resource",
+                requires_changes=bool(change_details.proposed_changes),
+                **log_params,
+            )
+        return change_details
 
     @property
     def resource_type(self):
--- iambic/plugins/v0_1_0/google_workspace/user/utils.py
+++ iambic/plugins/v0_1_0/google_workspace/user/utils.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
+from urllib.error import HTTPError
 
 from googleapiclient import _auth
 
+from iambic.core.context import ctx
 from iambic.core.logger import log
+from iambic.core.models import ProposedChange, ProposedChangeType
 from iambic.core.utils import aio_wrapper
 from iambic.plugins.v0_1_0.google_workspace.user.models import (
     GoogleWorkspaceUserTemplate,
@@ -43,3 +46,188 @@ async def list_users(
         # handle pagination based on https://googleapis.github.io/google-api-python-client/docs/pagination.html
         req = await aio_wrapper(service.users().list_next, req, res)
     return users
+
+
+async def get_user(user_email: str, domain: str, google_project: GoogleProject):
+    from iambic.plugins.v0_1_0.google_workspace.user.models import (
+        get_user_template,  # Adjust import based on your setup
+    )
+
+    try:
+        service = await google_project.get_service_connection(
+            "admin", "directory_v1", domain
+        )
+        if not service:
+            return []
+        http = _auth.authorized_http(service._http.credentials)
+    except AttributeError as err:
+        log.exception("Unable to process google users.", error=err)
+        return
+
+    req = service.users().get(userKey=user_email)
+    try:
+        if user := req.execute(http=http):
+            return await get_user_template(service, user, domain)
+    except HTTPError as err:
+        log.error("Unable to get user. It may not exist", error=err.reason)
+
+
+async def create_user(
+    primary_email: str, name: dict, domain: str, google_project: GoogleProject
+):
+    try:
+        service = await google_project.get_service_connection(
+            "admin", "directory_v1", domain
+        )
+        if not service:
+            return []
+        http = _auth.authorized_http(service._http.credentials)
+    except AttributeError as err:
+        log.exception("Unable to process google users.", error=err)
+        return
+
+    body = {"primaryEmail": primary_email, "name": name}
+
+    req = await aio_wrapper(
+        service.users().insert,
+        body=body,
+    )
+    return req.execute(http=http)
+
+
+async def update_user_email(
+    current_email: str, proposed_email: str, log_params: dict[str, str]
+) -> list[ProposedChange]:
+    response: list[ProposedChange] = []
+    if current_email == proposed_email:
+        return response
+
+    log_str = "Detected updated user email"
+    response.append(
+        ProposedChange(
+            change_type=ProposedChangeType.UPDATE,
+            resource_id=current_email,
+            resource_type="google:user:template",
+            attribute="email",
+            change_summary={
+                "current_email": current_email,
+                "proposed_email": proposed_email,
+            },
+            current_value=current_email,
+            new_value=proposed_email,
+        )
+    )
+    if ctx.execute:
+        log_str = "Updating user email"
+        # Update user email logic
+    log.info(
+        log_str,
+        current_email=current_email,
+        proposed_email=proposed_email,
+        **log_params,
+    )
+    return response
+
+
+async def maybe_delete_user(
+    user: GoogleWorkspaceUserTemplate,
+    google_project: GoogleProject,
+    log_params: dict[str, str],
+) -> list[ProposedChange]:
+    response: list[ProposedChange] = []
+
+    # If the user is not marked for deletion, return an empty response.
+    if not user.deleted:
+        return response
+
+    # Create a proposed change object indicating that the user should be deleted.
+    response.append(
+        ProposedChange(
+            change_type=ProposedChangeType.DELETE,
+            resource_id=user.properties.primary_email,
+            resource_type=user.resource_type,
+            attribute="user",
+            change_summary={"user": user.properties.name},
+            current_value=user.properties.name,
+        )
+    )
+
+    # If execution is enabled, proceed to delete the user.
+    if ctx.execute:
+        try:
+            service = await google_project.get_service_connection(
+                "admin", "directory_v1", user.properties.domain
+            )
+            if not service:
+                return []
+            http = _auth.authorized_http(service._http.credentials)
+        except AttributeError as err:
+            log.exception("Unable to process google users.", error=err)
+            return []
+
+        log_str = "Detected user deletion"
+
+        if ctx.execute:
+            log_str = "Deleting User"
+            await aio_wrapper(
+                service.users().delete(userKey=user.properties.primary_email).execute,
+                http=http,
+            )
+        log.info(log_str, user_email=user.properties.primary_email, **log_params)
+    return response
+
+
+async def update_user_name(
+    user_email: str,
+    current_name: dict,
+    proposed_name: dict,
+    domain: str,
+    google_project: GoogleProject,
+    log_params: dict[str, str],
+) -> list[ProposedChange]:
+    response: list[ProposedChange] = []
+
+    # Check if the current name and proposed name are the same.
+    if current_name == proposed_name:
+        return response
+
+    # Log and record a proposed change if the names differ.
+    log_str = "Detected user name update"
+    response.append(
+        ProposedChange(
+            change_type=ProposedChangeType.UPDATE,
+            resource_id=user_email,
+            resource_type="google:user:template",
+            attribute="user_name",
+            current_value=current_name,
+            new_value=proposed_name,
+        )
+    )
+
+    # Connect to the Google service.
+    try:
+        service = await google_project.get_service_connection(
+            "admin", "directory_v1", domain
+        )
+        if not service:
+            return response
+        http = _auth.authorized_http(service._http.credentials)
+    except AttributeError as err:
+        log.exception("Unable to process google users.", error=err)
+        return response
+
+    # If execution is enabled, update the user's name.
+    if ctx.execute:
+        log_str = "Updating user name"
+        await aio_wrapper(
+            service.users()
+            .patch(userKey=user_email, body={"name": proposed_name})
+            .execute,
+            http=http,
+        )
+
+    log.info(
+        log_str, current_name=current_name, proposed_name=proposed_name, **log_params
+    )
+
+    return response
